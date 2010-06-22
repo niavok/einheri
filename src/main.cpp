cae1ec2309dfdb150f4e2416881e343c788711a1
Frédéric Bertolus <fred.bertolus@gmail.com>
2010-06-22 20:21:02 +0200

    add main

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: fred
+ *
+ * Created on 22 juin 2010, 20:08
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
