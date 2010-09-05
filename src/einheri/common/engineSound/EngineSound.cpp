beba9657b8be01ae10e13c62b2b48ca155754a58
Frederic Bertolus <fred.bertolus@gmail.com>
2010-09-06 01:00:17 +0200

    ajout d'un son lors des tirs

--- src/einheri/common/engineSound/EngineSound.cpp
+++ src/einheri/common/engineSound/EngineSound.cpp
@@ -10,44 +10,96 @@
 #include <SFML/Audio.hpp>
 #include <einheri/utils/FileFinder.h>
 #include <einheri/utils/Log.h>
+#include <einheri/common/event/EventVisitor.h>
 
 using einUtils::FileFinder;
 
 namespace ein {
 
-EngineSound::EngineSound(GameManager* manager) : Engine(manager){
+EngineSound::EngineSound(GameManager* manager) :
+    FramerateEngine(manager) {
     music = NULL;
+    UseGameTime(false);
+    UseSoftFrameRate(true);
+
+
+    fireSoundBuffer.LoadFromFile(FileFinder::get().file("share/m16.ogg"));
 }
 
 EngineSound::~EngineSound() {
-    if(music) {
+    if (music) {
         music->Stop();
         delete music;
     }
+    while(!sounds.empty()) {
+        sf::Sound* sound = sounds.front();
+        if(sound) {
+            sound->Stop();
+            delete sound;
+        }
+        sounds.pop_front();
+    }
+
 }
 
-void EngineSound::Apply(const Event& /*event*/) {
+void EngineSound::Apply(const Event& event) {
+    class EngineSoundVisitor: public EventVisitor {
+    public:
+        EngineSoundVisitor(EngineSound* engine) :
+            engine(engine) {
+        }
+
+        void Visit(const EventProjectileAdded& eventProjectileAdded) {
+            engine->processEventProjectileAdded(eventProjectileAdded);
+        }
+
+    private:
+        EngineSound* engine;
+    };
+    EngineSoundVisitor visitor(this);
+    event.accept(visitor);
 }
 
-void EngineSound::Frame() {
-    if(!music) {
+void EngineSound::frame(EinValue) {
+    if (!music) {
 
-    // Load a music to play
-         music = new sf::Music();
-         //if (!Music.OpenFromFile(FileFinder::get().file("share/Olivier_Militon.-.Apnee.ogg")))
+        // Load a music to play
+        music = new sf::Music();
 
-         if (!music->OpenFromFile("/home/fred/projets/einheri/files/share/Olivier_Militon.-.Apnee.ogg"))
-             return;
+        if (!music->OpenFromFile(FileFinder::get().file("share/Olivier_Militon.-.Apnee.ogg")))
+            return;
 
-         LOG("playMusic");
-         // Play the music
-         music->Play();
-    } else if(music->GetStatus() != sf::Music::Playing) {
+        LOG("playMusic");
+        // Play the music
+        music->Play();
+    } else if (music->GetStatus() != sf::Music::Playing) {
 
         delete music;
         music = NULL;
     }
 
+    std::list<sf::Sound*>::iterator it;
+
+    for(it = sounds.begin(); it != sounds.end(); ++it) {
+        sf::Sound*& sound = *it;
+        if(sound->GetStatus()  != sf::Sound::Playing) {
+
+            delete sound;
+            sound = NULL;
+        }
+    }
+
+    while(sounds.front() == NULL) {
+        sounds.pop_front();
+    }
+
+}
+
+void EngineSound::processEventProjectileAdded(const EventProjectileAdded& eventProjectileAdded) {
+    sf::Sound* sound = new sf::Sound();
+    sound->SetBuffer(fireSoundBuffer);
+    sound->Play();
+    sounds.push_back(sound);
 }
 
 }
--- src/einheri/common/engineSound/EngineSound.h
+++ src/einheri/common/engineSound/EngineSound.h
@@ -8,22 +8,31 @@
 #ifndef EIN_ENGINESOUND_H_
 #define EIN_ENGINESOUND_H_
 
-#include <einheri/common/Engine.h>
+#include <einheri/common/FramerateEngine.h>
 #include <SFML/Audio.hpp>
+#include <list>
+#include "einheri/common/event/EventProjectileAdded.h"
 
 namespace ein {
 
 class Event;
 
-class EngineSound : public Engine{
+class EngineSound : public FramerateEngine{
 public:
     EngineSound(GameManager* manager);
     virtual ~EngineSound();
 
     void Apply(const Event& event);
-    void Frame();
+private:
+    void frame(EinValue framerate);
+
+    void processEventProjectileAdded(const EventProjectileAdded& eventProjectileAdded);
+
 private:
     sf::Music *music;
+    std::list<sf::Sound*> sounds;
+    sf::SoundBuffer fireSoundBuffer;
+
 };
 
 }
